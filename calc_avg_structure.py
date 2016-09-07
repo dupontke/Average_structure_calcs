e7f270df78da643da7d6b3f75c4166dd7c23c8a8
Russell Bruce Davidson <RBD@Russells-MacBook-Pro.local>
2016-09-06 22:50:07 -0600

    added a config file that holds all the necessary variables to run this script; I have given a lot mroe freedom to the user to run this script; not sure if that was smart or not...

--- calc_avg_structure.py
+++ calc_avg_structure.py
@@ -1,6 +1,8 @@
 #!/Library/Frameworks/Python.framework/Versions/2.7/bin/python
 # ----------------------------------------
 # USAGE:
+# ./calc_avg_structure.py pdb_file trajectory_location 1st_trajectory_# last_trajectory_# system_descripter avg_struct_filename
+# ./calc_avg_structure.py config_file
 
 # ----------------------------------------
 # PREAMBLE:
@@ -12,60 +14,82 @@ import MDAnalysis
 from MDAnalysis.analysis.align import *
 from distance_functions import *
 
-# ----------------------------------------
-# VARIABLE DECLARATION
-
-pdb_file = sys.argv[1]
-traj_loc = sys.argv[2]
-start = int(sys.argv[3])
-end = int(sys.argv[4])
-out_file = sys.argv[5]
-
-alignment = 'protein and name CA and (resid 20:25 or resid 50:55 or resid 73:75 or resid 90:94 or resid 112:116 or resid 142:147 or resid 165:169 or resid 190:194 or resid 214:218 or resid 236:240 or resid 253:258 or resid 303:307)'
-
 zeros = np.zeros
 dot_prod = np.dot
 sqrt = np.sqrt
 flush = sys.stdout.flush
 
-thresh = 1E-5
-maxIter = 100
+# ----------------------------------------
+# VARIABLE DECLARATION
+
+config_file = sys.argv[1]	# Local or Global positon of the config file that holds all the values for the parameters
+
+necessary_parameters = [pdb_file,traj_loc,start,end,system,Wrapped]  # LEAVING OUT alignment, thresh, maxIter variables...
 
 # ----------------------------------------
 # SUBROUTINES:
 
-def ffprint(string):
+def ffprint(string):		# Useful function to use when on a system that has a large buffer memory
 	print '%s' %(string)
 	flush()
 
+def config_parser(config_file):	# Function to take config file and create/fill the parameter dictionary 
+	parameters = {}
+	for i in range(len(necessary_parameters)):
+		parameters[necessary_parameters[i]] = ''
+
+	# SETTING DEFAULT PARAMETERS FOR OPTIONAL PARAMETERS:
+	parameters['alignment'] = 'protein'
+	parameters['important'] = 'protein'
+	parameters['substrate'] = 'protein'	# GENERALLY A VERY BAD SELECTION FOR THE SUBSTRATE... 
+	parameters['thresh'] = 1E-5
+	parameters['maxIter'] = 100
+	parameters['write_dcd'] = False
+	parameters['write_summary'] = False
+	parameters['write_overview'] = False
+	
+	# GRABBING PARAMETER VALUES FROM THE CONFIG FILE:
+	execfile(config_file,parameters)
+	for key, value in parameters.iteritems:
+		if value == '':
+			print '%s has not been assigned a value. This variable is necessary for the script to run. Please declare this variable within the config file.'
+			sys.exit()
+
+def summary():
+	with open('%s.avg_structure.summary' %(system),'w') as f:
+		### output all the parameters used... need to ignore the __builtins__ dictionary key... ugh...
+
 # ----------------------------------------
 # MAIN PROGRAM:
-
-# LOAD IN THE UNIVERSE TO BE ANALYZED AND INITIATE THE NECESSARY ATOM SELECTIONS
-u = MDAnalysis.Universe(pdb_file)
-u_all = u.select_atoms('all')
-u_align = u.select_atoms(alignment)
-u_important = u.select_atoms('protein or nucleic or resname A5 or resname A3 or resname U5 or resname atp or resname adp or resname PHX or resname MG')
-u_substrate = u.select_atoms('nucleic or resname A5 or resname A3 or resname U5 or resname atp or resname adp or resname PHX or resname MG')
+# CREATING PARAMETER DICTIONARY
+config_parser(config_file)
 
 # INITIATE THE PDB FILE TO BE USED TO SAVE THE AVERAGE STRUCTURE
-avg_pdb = MDAnalysis.Universe(pdb_file)
-avg_important = avg_pdb.select_atoms('protein or nucleic or resname A5 or resname A3 or resname U5 or resname atp or resname adp or resname PHX or resname MG')
+avg_pdb = MDAnalysis.Universe(parameters['pdb_file'])
+avg_important = avg_pdb.select_atoms(parameters['important'])
+
+# LOAD IN THE UNIVERSE TO BE ANALYZED AND INITIATE THE NECESSARY ATOM SELECTIONS
+u = MDAnalysis.Universe(parameters['pdb_file'])
+u_align = u.select_atoms(parameters['alignment'])
+u_important = u.select_atoms(parameters['important'])
+if parameters['Wrapped'] != True:
+	u_substrate = u.select_atoms(parameters['substrate'])
 
 # GRABBING IMPORTANT NUMBERS FROM THE UNIVERSE
 u_important_atoms = u_important.n_atoms  	#len(u_important.atoms)
 u_align_atoms = u_align.n_atoms			#len(u_align.atoms)
-u_substrate_res = u_substrate.n_residues	#len(u_substrate.residues)
+if parameters['Wrapped'] != True:
+	u_substrate_res = u_substrate.n_residues	#len(u_substrate.residues)
 
 # DETERMINING THE NUMBER OF STEPS TO BE AVERAGED OVER
 nSteps = 0
-while start <= end:
-	u.load_new('%sproduction.%s/production.%s.dcd' %(traj_loc,start,start))
+start = parameters['start']
+while start <= parameters['end']:
+	u.load_new('%sproduction.%s/production.%s.dcd' %(parameters['traj_loc'],start,start))
 	nSteps += len(u.trajectory)
 	start += 1
 
 ffprint('Number of steps to be averaged over : %d' %(nSteps))
-start = int(sys.argv[3])
 
 # ARRAY DECLARATION
 all_coord = zeros((nSteps,u_important_atoms,3),dtype=np.float64)
@@ -75,21 +99,24 @@ avgAlign = zeros((u_align_atoms,3),dtype=np.float64)
 
 # Trajectory Analysis: 
 ffprint('Beginning trajectory analysis')
+start = parameters['start']
 temp = 0 
-while start <= end:
+while start <= parameters['end']:
 	ffprint('Loading trajectory %s' %(start))
-	u.load_new('%sproduction.%s/production.%s.dcd' %(traj_loc,start,start))
+	u.load_new('%sproduction.%s/production.%s.dcd' %(parameters['traj_loc'],start,start))
 
 	for ts in u.trajectory:
-		dims = u.dimensions[:3]
-		dims2 = dims/2.0
+		u_important.translate(-u_align.center_of_mass())
+		
+		if parameters['Wrapped'] != True:
+			# CALCULATIONS that are unnecessary if the trajectory is wrapped.
+			dims = u.dimensions[:3]		
+			dims2 = dims/2.0
 
-		u_all.translate(-u_align.center_of_mass())
-
-		for i in range(u_substrate_res):
-			COM = u_substrate.residues[i].center_of_mass()
-			t = wrapping(COM,dims,dims2)
-			u_substrate.residues[i].atoms.translate(t)
+			for i in range(u_substrate_res):
+				COM = u_substrate.residues[i].center_of_mass()
+				t = wrapping(COM,dims,dims2)
+				u_substrate.residues[i].atoms.translate(t)
 
 		avgCoord += u_important.positions
 		avgAlign += u_align.positions
@@ -105,15 +132,13 @@ if temp != nSteps:
 
 avgCoord /= float(nSteps)
 avgAlign /= float(nSteps)
-ffprint(len(all_coord))
-ffprint(len(all_coord[0]))
 ffprint('Finished with the trajectory analysis')
 
 # Calculating and Aligning to the average positions
 iteration = 0
-residual = thresh + 10.0 					# arbitrary assignment greater than thresh
+residual = parameters['thresh'] + 10.0 					# arbitrary assignment greater than thresh
 ffprint('Beginning iterative process of calculating average positions and aligning to the average')
-while residual > thresh and iteration < maxIter:		
+while residual > parameters['thresh'] and iteration < parameters['maxIter']:
 	tempAvgCoord = zeros((u_important_atoms,3),dtype=np.float64)		# zeroing out the tempAvgCoord array every iteration
 	tempAvgAlign = zeros((u_align_atoms,3),dtype=np.float64)
 	for i in range(nSteps):
@@ -135,16 +160,17 @@ ffprint('Average structure has converged')				# Now have the iteratively aligned
 # Print out pdb of average structure
 ffprint('Writing a pdb of the average structure.')
 avg_important.positions = avgCoord
-avg_important.write('%03d.%03d.avg_structure.pdb' %(int(sys.argv[3]),end))
+avg_important.write('%03d.%03d.%s.avg_structure.pdb' %(parameters['start'],parameters['end'],parameters['system']))
 ffprint('Finished writing pdb of the average structure')
 
 # PRINT out dcd frame of average structure; has more precision than the pdb format
-ffprint('Writing a dcd frame of the average structure.')
-avg_important.write('%03d.%03d.avg_structure.dcd' %(int(sys.argv[3]),end))
-ffprint('Finished writing dcd of the average structure')
+if parameters['write_dcd'] == True:
+	ffprint('Writing a dcd frame of the average structure.')
+	avg_important.write('%03d.%03d.%s.avg_structure.dcd' %(parameters['start'],parameters['end']),parameters['system'])
+	ffprint('Finished writing dcd of the average structure')
 
-# APPENDING INFORMATION TO A SUMMARY FILE
-out = open('%s' %(out_file),'a')
-out.write('%d   %d   %d\n' %(int(sys.argv[3]), end, nSteps))
-out.close()
+# APPENDING INFORMATION TO THE OVERVIEW FILE
+if parameters['write_overview'] == True:
+	with open('%s_averaging.output' %(parameters['system']),'a') as f:
+		f.write('%d   %d   %d\n' %(parameters['start'],parameters['end'], nSteps))
 
