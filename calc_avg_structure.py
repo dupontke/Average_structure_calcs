c462c7867ca2c6a5b39454c5916334e34148f21f
Kelly Du Pont <kelly.dupont@colostate.edu>
2019-11-08 13:14:30 -0700

    updated in November 2019

--- calc_avg_structure.py
+++ calc_avg_structure.py
@@ -21,7 +21,7 @@ flush = sys.stdout.flush
 # ----------------------------------------
 # VARIABLE DECLARATION
 
-config_file = sys.argv[1]	# Local or Global positon of the config file that holds all the values for the parameters
+config_file = sys.argv[1]# Local or Global positon of the config file that holds all the values for the parameters
 
 necessary_parameters = ['pdb_file','traj_loc','start','end','Wrapped','avg_filename']  # LEAVING OUT alignment, thresh, maxIter variables...
 all_parameters = ['pdb_file','traj_loc','start','end','Wrapped','alignment','important','substrate','thresh','maxIter','write_dcd','write_summary','write_overview','overview_filename']
@@ -30,42 +30,42 @@ parameters = {}
 # ----------------------------------------
 # SUBROUTINES:
 
-def ffprint(string):		# Useful function to use when on a system that has a large buffer memory
-	print '%s' %(string)
-	flush()
+def ffprint(string):# Useful function to use when on a system that has a large buffer memory
+    print '%s' %(string)
+    flush()
 
-def config_parser(config_file):	# Function to take config file and create/fill the parameter dictionary 
-	for i in range(len(necessary_parameters)):
-		parameters[necessary_parameters[i]] = ''
+def config_parser(config_file):# Function to take config file and create/fill the parameter dictionary
+    for i in range(len(necessary_parameters)):
+        parameters[necessary_parameters[i]] = ''
 
-	# SETTING DEFAULT PARAMETERS FOR OPTIONAL PARAMETERS:
-	parameters['alignment'] = 'protein'
-	parameters['important'] = 'protein'
-	parameters['substrate'] = 'protein'	# GENERALLY A VERY BAD SELECTION FOR THE SUBSTRATE... 
-	parameters['thresh'] = 1E-5
-	parameters['maxIter'] = 100
-	parameters['write_dcd'] = False
-	parameters['write_summary'] = False
-	parameters['write_overview'] = False
-	parameters['overview_filename'] = None
-	
-	# GRABBING PARAMETER VALUES FROM THE CONFIG FILE:
-	execfile(config_file,parameters)
-	for key, value in parameters.iteritems():
-		if value == '':
-			print '%s has not been assigned a value. This variable is necessary for the script to run. Please declare this variable within the config file.'
-			sys.exit()
+    # SETTING DEFAULT PARAMETERS FOR OPTIONAL PARAMETERS:
+    parameters['alignment'] = 'protein'
+    parameters['important'] = 'protein'
+    parameters['substrate'] = 'protein'# GENERALLY A VERY BAD SELECTION FOR THE SUBSTRATE... 
+    parameters['thresh'] = 1E-5
+    parameters['maxIter'] = 100
+    parameters['write_dcd'] = False
+    parameters['write_summary'] = False
+    parameters['write_overview'] = False
+    parameters['overview_filename'] = None
+            
+    # GRABBING PARAMETER VALUES FROM THE CONFIG FILE:
+    execfile(config_file,parameters)
+    for key, value in parameters.iteritems():
+        if value == '':
+            print '%s has not been assigned a value. This variable is necessary for the script to run. Please declare this variable within the config file.'
+            sys.exit()
 
 def summary():
-	with open('%s.summary' %(parameters['avg_filename']),'w') as f:
-		f.write('Using MDAnalysis version: %s\n' %(MDAnalysis.version.__version__))
-		f.write('To recreate this analysis, run this line:\n')
-		for i in range(len(sys.argv)):
-			f.write('%s ' %(sys.argv[i]))
-		f.write('\nParameters used:\n')
-		for i in all_parameters:
-			f.write('%s = %s \n' %(i, parameters[i]))
-		f.write('\n\n')
+    with open('%s.summary' %(parameters['avg_filename']),'w') as f:
+        f.write('Using MDAnalysis version: %s\n' %(MDAnalysis.version.__version__))
+        f.write('To recreate this analysis, run this line:\n')
+        for i in range(len(sys.argv)):
+            f.write('%s ' %(sys.argv[i]))
+        f.write('\nParameters used:\n')
+        for i in all_parameters:
+            f.write('%s = %s \n' %(i, parameters[i]))
+        f.write('\n\n')
 
 # ----------------------------------------
 # MAIN PROGRAM:
@@ -81,19 +81,19 @@ u = MDAnalysis.Universe(parameters['pdb_file'])
 u_align = u.select_atoms(parameters['alignment'])
 u_important = u.select_atoms(parameters['important'])
 # GRABBING IMPORTANT NUMBERS FROM THE UNIVERSE
-u_important_atoms = u_important.n_atoms  	#len(u_important.atoms)
-u_align_atoms = u_align.n_atoms			#len(u_align.atoms)
-if not parameters['Wrapped']:		# Test to see if the 'Wrapped' key is equal to False
-	u_substrate = u.select_atoms(parameters['substrate'])
-	u_substrate_res = u_substrate.n_residues	#len(u_substrate.residues)
+u_important_atoms = u_important.n_atoms         #len(u_important.atoms)
+u_align_atoms = u_align.n_atoms                 #len(u_align.atoms)
+if not parameters['Wrapped']:          # Test to see if the 'Wrapped' key is equal to False
+    u_substrate = u.select_atoms(parameters['substrate'])
+    u_substrate_res = u_substrate.n_residues        #len(u_substrate.residues)
 
 # DETERMINING THE NUMBER OF STEPS TO BE AVERAGED OVER
 nSteps = 0
 start = parameters['start']
 while start <= parameters['end']:
-	u.load_new('%sproduction.%s/production.%s.dcd' %(parameters['traj_loc'],start,start))
-	nSteps += len(u.trajectory)
-	start += 1
+    u.load_new('%sproduction.%s/production.%s.dcd' %(parameters['traj_loc'],start,start))
+    nSteps += len(u.trajectory)
+    start += 1
 
 ffprint('Number of steps to be averaged over : %d' %(nSteps))
 
@@ -108,33 +108,33 @@ ffprint('Beginning trajectory analysis')
 start = parameters['start']
 temp = 0 
 while start <= parameters['end']:
-	ffprint('Loading trajectory %s' %(start))
-	u.load_new('%sproduction.%s/production.%s.dcd' %(parameters['traj_loc'],start,start))
+    ffprint('Loading trajectory %s' %(start))
+    u.load_new('%sproduction.%s/production.%s.dcd' %(parameters['traj_loc'],start,start))
 
-	for ts in u.trajectory:
-		u_important.translate(-u_align.center_of_mass())
-		
-		if not parameters['Wrapped']:		# Test to see if the 'Wrapped' key is equal to False
-			# CALCULATIONS that are unnecessary if the trajectory is wrapped.
-			dims = u.dimensions[:3]		
-			dims2 = dims/2.0
+    for ts in u.trajectory:
+        u_important.translate(-u_align.center_of_mass())
 
-			for i in range(u_substrate_res):
-				COM = u_substrate.residues[i].center_of_mass()
-				t = wrapping(COM,dims,dims2)
-				u_substrate.residues[i].atoms.translate(t)
+        if not parameters['Wrapped']:          # Test to see if the 'Wrapped' key is equal to False 
+            # CALCULATIONS that are unnecessary if the trajectory is wrapped.
+            dims = u.dimensions[:3]
+            dims2 = dims/2.0
 
-		avgCoord += u_important.positions
-		avgAlign += u_align.positions
-		all_coord[temp] = u_important.positions
-		all_align[temp] = u_align.positions
-		temp += 1
-	start += 1
+            for i in range(u_substrate_res):
+                COM = u_substrate.residues[i].center_of_mass()
+                t = wrapping(COM,dims,dims2)
+                u_substrate.residues[i].atoms.translate(t)
+
+        avgCoord += u_important.positions
+        avgAlign += u_align.positions
+        all_coord[temp] = u_important.positions
+        all_align[temp] = u_align.positions
+        temp += 1
+    start += 1
 
 ffprint(nSteps)
 if temp != nSteps:
-	ffprint('Failed to analyze all timesteps.')
-	sys.exit()
+    ffprint('Failed to analyze all timesteps.')
+    sys.exit()
 
 avgCoord /= float(nSteps)
 avgAlign /= float(nSteps)
@@ -142,26 +142,26 @@ ffprint('Finished with the trajectory analysis')
 
 # Calculating and Aligning to the average positions
 iteration = 0
-residual = parameters['thresh'] + 10.0 					# arbitrary assignment greater than thresh
+residual = parameters['thresh'] + 10.0                                    # arbitrary assignment greater than thresh
 ffprint('Beginning iterative process of calculating average positions and aligning to the average')
 while residual > parameters['thresh'] and iteration < parameters['maxIter']:
-	tempAvgCoord = zeros((u_important_atoms,3),dtype=np.float64)		# zeroing out the tempAvgCoord array every iteration
-	tempAvgAlign = zeros((u_align_atoms,3),dtype=np.float64)
-	for i in range(nSteps):
-		R, d = rotation_matrix(all_align[i,:,:],avgAlign)
-		all_align[i,:,:] = dot_prod(all_align[i,:,:],R.T)
-		all_coord[i,:,:] = dot_prod(all_coord[i,:,:],R.T)
-		tempAvgAlign += all_align[i,:,:]
-		tempAvgCoord += all_coord[i,:,:]			# recalculate the average coordinates to optimize the average position
-	tempAvgCoord /= float(nSteps)				# finishing the average
-	tempAvgAlign /= float(nSteps)
-	residual = RMSD(avgAlign, tempAvgAlign, u_align_atoms)			# calculating the rmsd between avg and tempAvg to quantify our iterative optimization of the average positions	
-	rmsd_all = RMSD(avgCoord, tempAvgCoord, u_important_atoms)
-	iteration += 1
-	avgCoord = tempAvgCoord
-	avgAlign = tempAvgAlign
-	ffprint('Steps: %d, RMSD btw alignment atoms: %e, RMSD btw all atoms: %e' %(iteration,residual,rmsd_all))
-ffprint('Average structure has converged')				# Now have the iteratively aligned avgCoord array, as well as the iteratively aligned (COG-corrected and rotated) allCoord array
+    tempAvgCoord = zeros((u_important_atoms,3),dtype=np.float64)                # zeroing out the tempAvgCoord array every iteration
+    tempAvgAlign = zeros((u_align_atoms,3),dtype=np.float64)
+    for i in range(nSteps):
+        R, d = rotation_matrix(all_align[i,:,:],avgAlign)
+        all_align[i,:,:] = dot_prod(all_align[i,:,:],R.T)
+        all_coord[i,:,:] = dot_prod(all_coord[i,:,:],R.T)
+        tempAvgAlign += all_align[i,:,:]
+        tempAvgCoord += all_coord[i,:,:]                        # recalculate the average coordinates to optimize the average position
+    tempAvgCoord /= float(nSteps)                               # finishing the average
+    tempAvgAlign /= float(nSteps)
+    residual = RMSD(avgAlign, tempAvgAlign, u_align_atoms)                  # calculating the rmsd between avg and tempAvg to quantify our iterative optimization of the average positions
+    rmsd_all = RMSD(avgCoord, tempAvgCoord, u_important_atoms)
+    iteration += 1
+    avgCoord = tempAvgCoord
+    avgAlign = tempAvgAlign
+    ffprint('Steps: %d, RMSD btw alignment atoms: %e, RMSD btw all atoms: %e' %(iteration,residual,rmsd_all))
+ffprint('Average structure has converged')                        # Now have the iteratively aligned avgCoord array, as well as the iteratively aligned (COG-corrected and rotated) allCoord array
 
 # Print out pdb of average structure
 ffprint('Writing a pdb of the average structure.')
@@ -170,17 +170,16 @@ avg_important.write('%s.pdb' %(parameters['avg_filename']))
 ffprint('Finished writing pdb of the average structure')
 
 # PRINT out dcd frame of average structure; has more precision than the pdb format
-if parameters['write_dcd']:		# Test if 'write_dcd' key is equal to True
-	ffprint('Writing a dcd frame of the average structure.')
-	with MDAnalysis.Writer('%s.dcd' %(parameters['avg_filename']),avg_important.n_atoms) as W:
-		W.write(avg_important)
-	ffprint('Finished writing dcd of the average structure')
+if parameters['write_dcd']:            # Test if 'write_dcd' key is equal to True
+    ffprint('Writing a dcd frame of the average structure.')
+    with MDAnalysis.Writer('%s.dcd' %(parameters['avg_filename']),avg_important.n_atoms) as W:
+        W.write(avg_important)
+    ffprint('Finished writing dcd of the average structure')
 
 # APPENDING INFORMATION TO THE OVERVIEW FILE
-if parameters['write_overview']:	# Test if 'write_overview' key is equal to True
-	with open('../%s' %(parameters['overview_filename']),'a') as f:
-		f.write('%d   %d   %d\n' %(parameters['start'],parameters['end'], nSteps))
+if parameters['write_overview']:# Test if 'write_overview' key is equal to True
+    with open('../%s' %(parameters['overview_filename']),'a') as f:
+        f.write('%d   %d   %d\n' %(parameters['start'],parameters['end'], nSteps))
 
-if parameters['write_summary']:		# Test if 'write_summary' key is equal to True
-	summary()
-
+if parameters['write_summary']:           # Test if 'write_summary' key is equal to True
+    summary()
--- weighted_average.py
+++ weighted_average.py
@@ -15,11 +15,11 @@ from distance_functions import *
 # ----------------------------------------
 # VARIABLE DECLARATION
 
-input_file = sys.argv[1]		# Format: 	start_traj	end_traj	number_of_frames
-pdb_file = sys.argv[2]			# Read in a pdb file to be used for atom selections and assigning positions
-system = sys.argv[3]                    # system of trajectory
+input_file = sys.argv[1]# Format: start_trajend_trajnumber_of_frames
+pdb_file = sys.argv[2]# Read in a pdb file to be used for atom selections and assigning positions
+system = sys.argv[3]
 
-alignment = 'protein and name CA and (resid 15:23 or resid 50:60 or resid 68:73 or resid 79:86 or resid 90:96 or resid 117:120 or resid 135:138 or resid 150:161 or resid 170:175 or resid 182:193 or resid 197:199 or resid 211:214 or resid 222:236)'
+alignment = 'protein and name CA and (resid 20:25 or resid 50:55 or resid 73:75 or resid 90:94 or resid 112:116 or resid 142:147 or resid 165:169 or resid 190:194 or resid 214:218 or resid 236:240 or resid 253:258 or resid 303:307)'
 
 zeros = np.zeros
 dot_prod = np.dot
@@ -33,8 +33,8 @@ maxIter = 100
 # SUBROUTINES:
 
 def ffprint(string):
-	print '%s' %(string)
-	flush()
+    print '%s' %(string)
+    flush()
 
 # ----------------------------------------
 # MAIN PROGRAM:
@@ -49,14 +49,14 @@ nSteps = np.sum(average_list[:,2])
 u = MDAnalysis.Universe(pdb_file)
 u_all = u.select_atoms('all')
 u_align = u.select_atoms(alignment)
-u_important = u.select_atoms('protein or resname GTP or resname SAH or resname MG')
-u_substrate = u.select_atoms('resname GTP or resname SAH or resname MG')
+u_important = u.select_atoms('protein or nucleic or resname A5 or resname A3 or resname U5 or resname ATP or resname ADP or resname PHX or resname MG')
+u_substrate = u.select_atoms('nucleic or resname A5 or resname A3 or resname U5 or resname ATP or resname ADP or resname PHX or resname MG')
 pos0 = u_align.positions
 
 # GRABBING IMPORTANT NUMBERS FROM THE UNIVERSE
-u_important_atoms = u_important.n_atoms		#len(u_important.atoms)
-u_align_atoms = u_align.n_atoms			#len(u_align.atoms)
-u_substrate_res = u_substrate.n_residues	#len(u_substrate.residues)
+u_important_atoms = u_important.n_atoms#len(u_important.atoms)
+u_align_atoms = u_align.n_atoms#len(u_align.atoms)
+u_substrate_res = u_substrate.n_residues#len(u_substrate.residues)
 
 # ARRAY DECLARATION
 all_coord = zeros((nAverages,u_important_atoms,3),dtype=np.float64)
@@ -67,22 +67,22 @@ avgAlign = zeros((u_align_atoms,3),dtype=np.float64)
 # AVERAGE STRUCTURE ANALYSIS
 ffprint('Beginning the averaging of averages process')
 for i in range(nAverages):
-	ffprint('Loading in the average structure of Trajectories %d to %d' %(average_list[i][0],average_list[i][1]))
-	temp = MDAnalysis.Universe('%03d.%03d.avg_structure/%03d.%03d.%s.avg_structure.pdb' %(average_list[i][0],average_list[i][1],average_list[i][0],average_list[i][1],system))
-	# INITIATING ATOM SELECTIONS
-	temp_all = temp.select_atoms('all')
-	temp_align = temp.select_atoms(alignment)
-	temp_important = temp.select_atoms('protein or resname GTP or resname SAH or resname MG')
-	temp_substrate = temp.select_atoms('resname GTP or resname SAH or resname MG')
+    ffprint('Loading in the average structure of Trajectories %d to %d' %(average_list[i][0],average_list[i][1]))
+    temp = MDAnalysis.Universe('%03d.%03d.avg_structure/%03d.%03d.%s.avg_structure.pdb' %(average_list[i][0],average_list[i][1],average_list[i][0],average_list[i][1],system))
+    # INITIATING ATOM SELECTIONS
+    temp_all = temp.select_atoms('all')
+    temp_align = temp.select_atoms(alignment)
+    temp_important = temp.select_atoms('protein or nucleic or resname A5 or resname A3 or resname U5 or resname ATP or resname ADP or resname PHX or resname MG')
+    temp_substrate = temp.select_atoms('nucleic or resname A5 or resname A3 or resname U5 or resname ATP or resname ADP or resname PHX or resname MG')
 
-	# TRANSLATING AVERAGE STRUCTURES TO THE PDB STRUCTURE
-	temp_all.translate(-temp_align.center_of_mass())
+    # TRANSLATING AVERAGE STRUCTURES TO THE PDB STRUCTURE
+    temp_all.translate(-temp_align.center_of_mass())
 
-	# UNWEIGHTING THE AVERAGE STRUCTURE
-	all_coord[i] = temp_important.positions
-	all_align[i] = temp_align.positions
-	avgCoord += temp_important.positions*average_list[i][2]
-	avgAlign += temp_align.positions*average_list[i][2]
+    # UNWEIGHTING THE AVERAGE STRUCTURE
+    all_coord[i] = temp_important.positions
+    all_align[i] = temp_align.positions
+    avgCoord += temp_important.positions*average_list[i][2]
+    avgAlign += temp_align.positions*average_list[i][2]
 
 avgCoord /= float(nSteps)
 avgAlign /= float(nSteps)
@@ -90,35 +90,34 @@ ffprint('Finished collecting the average coordinates and averaging the averages.
 
 # Calculating and Aligning to the average positions
 iteration = 0
-residual = thresh + 10.0 					# arbitrary assignment greater than thresh
+residual = thresh + 10.0 # arbitrary assignment greater than thresh
 ffprint('Beginning iterative process of calculating average positions and aligning to the average')
-while residual > thresh and iteration < maxIter:		
-	tempAvgCoord = zeros((u_important_atoms,3),dtype=np.float64)		# zeroing out the tempAvgCoord array every iteration
-	tempAvgAlign = zeros((u_align_atoms,3),dtype=np.float64)
-	for i in range(nAverages):
-		R, d = rotation_matrix(all_align[i,:,:],avgAlign)
-		all_align[i,:,:] = dot_prod(all_align[i,:,:],R.T)
-		all_coord[i,:,:] = dot_prod(all_coord[i,:,:],R.T)
-		tempAvgAlign += all_align[i,:,:]
-		tempAvgCoord += all_coord[i,:,:]
-	tempAvgCoord /= float(nAverages)
-	tempAvgAlign /= float(nAverages)
-	residual = RMSD(avgAlign,tempAvgAlign,u_align_atoms)
-	rmsd_all = RMSD(avgCoord,tempAvgCoord,u_important_atoms)
-	iteration += 1
-	avgCoord = tempAvgCoord
-	avgAlign = tempAvgAlign
-	ffprint('Steps: %d, RMSD btw alignment atoms: %e, RMSD btw all atoms: %e' %(iteration,residual,rmsd_all))
+while residual > thresh and iteration < maxIter:
+    tempAvgCoord = zeros((u_important_atoms,3),dtype=np.float64)# zeroing out the tempAvgCoord array every iteration
+    tempAvgAlign = zeros((u_align_atoms,3),dtype=np.float64)
+    for i in range(nAverages):
+        R, d = rotation_matrix(all_align[i,:,:],avgAlign)
+        all_align[i,:,:] = dot_prod(all_align[i,:,:],R.T)
+        all_coord[i,:,:] = dot_prod(all_coord[i,:,:],R.T)
+        tempAvgAlign += all_align[i,:,:]
+        tempAvgCoord += all_coord[i,:,:]
+    tempAvgCoord /= float(nAverages)
+    tempAvgAlign /= float(nAverages)
+    residual = RMSD(avgAlign,tempAvgAlign,u_align_atoms)
+    rmsd_all = RMSD(avgCoord,tempAvgCoord,u_important_atoms)
+    iteration += 1
+    avgCoord = tempAvgCoord
+    avgAlign = tempAvgAlign
+    ffprint('Steps: %d, RMSD btw alignment atoms: %e, RMSD btw all atoms: %e' %(iteration,residual,rmsd_all))
 
 ffprint('Average structure has converged')
 
 u_important.positions = avgCoord
 
 with MDAnalysis.Writer('%03d.%03d.average_structure.pdb' %(average_list[0][0],average_list[-1][1]),u_important_atoms) as W:
-	W.write(u_important)
+    W.write(u_important)
 ffprint('Finished writing pdb of the average structure')
 
 with MDAnalysis.Writer('%03d.%03d.average_structure.dcd' %(average_list[0][0],average_list[-1][1]),u_important_atoms) as W:
-	W.write(u_important)
+    W.write(u_important)
 ffprint('Finished writing dcd of the average structure')
-
